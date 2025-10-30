975435a8f7c98537fb6e2a345e57a71e37aa6247
Joaquin Gonzalez <joaquiinng@gmail.com>
2025-10-29 21:54:17 -0300

    Iniciando el frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,69 +1,23 @@
-import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import TablaUsuarios from "./TablaUsuarios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar";
+import Especialidades from "./components/Especialidades";
+import Medicos from "./components/Medicos";
+import Turnos from "./components/Turnos";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [usuarios, setUsuarios] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  // Para cargar tablaUsuarios
-  useEffect(() => {
-    fetch('http://127.0.0.1:8000/usuarios/')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Error al cargar usuarios');
-      }
-      return response.json();
-    })
-    .then(data => {
-      setUsuarios(data);
-      setLoading(false);
-    })
-    .catch(err => {
-      setError(err.message);
-      setLoading(false);
-    });
-  }, []);
-
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <Navbar />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<h3>Bienvenido al sistema de Turnos Médicos</h3>} />
+          <Route path="/especialidades" element={<Especialidades />} />
+          <Route path="/medicos" element={<Medicos />} />
+          <Route path="/turnos" element={<Turnos />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-
-      
-      <div style={{ marginTop: '2rem'}}>
-        <h2>Lista de Usuarios</h2>
-        {loading && <p>Cargando usuarios</p>}
-        {error && <p style={{color: 'red'}}> Error: {error}</p>}
-        {!loading && !error && <TablaUsuarios usuarios={usuarios} /> }
-      </div>
-
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
